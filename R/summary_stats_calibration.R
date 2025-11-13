805dcc02a2c16bf8f06b37f72d22ad5dbb21b32f
Zixuan Wu <115194385+ZixuanWu1@users.noreply.github.com>
2025-11-13 12:32:47 -0600

    Update summary_stats_calibration.R
    Update a more complete framework

--- R/summary_stats_calibration.R
+++ R/summary_stats_calibration.R
@@ -36,7 +36,7 @@ noise_correlation <- function(df, snp_list = NA){
 ##' @param r: assumption on correlation between T and NT. Can be zero, shared, or a provided vector.
 ##'
 ##' @return a data frame of snp id, calibrated beta and its standard error
-calibration_summary_stats <- function(df, rho = NA, family = "trio", r = "shared"){
+calibration_summary_stats_raw <- function(df, rho = NA, family = "trio", r = "shared"){
 
   n = df$N_int
   N = df$N_ext
@@ -80,20 +80,66 @@ calibration_summary_stats <- function(df, rho = NA, family = "trio", r = "shared
 
 
 
-# Example:
-#
-# Create summary stats
-# library(mvtnorm)
-# x = rmvnorm(100, mean = c(2, 0), sigma = rbind(c(1, .5), c(.5, 1)))
-#
-# df = cbind(1:100, x[,1], 1, x[,2], 1, rnorm(100, 0, 0.0001), 0.01, 100, 10000 )
-#
-#
-# df = data.frame(df)
-#
-# colnames(df) = c("SNP", "beta_int", "beta_int_var", "alpha_int", "alpha_int_var",
-#                  "alpha_ext", "alpha_ext_var", "N_int", "N_ext")
-#
-# Compute calibrated estimator
-# rho = sample_overlap(df, 1:100)
-# calibrated_estimator = calibration(df, rho)
+##' Estimate the correlation between external and internal trio gwas (i.e. sample overlap)
+##'
+##' @param df: a data frame with these columns: "SNP", "beta_int", "beta_int_var", "alpha_int", "alpha_int_var",
+##' "alpha_ext", "alpha_ext_var", "N_int", "N_ext" (N_int, N_ext won't be used if assumed shared r)
+##' @param rho: sample overlap ratio
+##' @param family: type of family structure. Either trio or sibling
+##' @param method: theoretical or empirical
+##' @param r: vector of correlation between T and NT for each SNP. Default is 0 (only used ifmethod = "theory" and family == "trio").
+##' @param pheno_cor: scalar of phenotype correlation between siblings. (only used if method == "theory" and family == "sibling").
+##' @param family_size: scalar of family size (only used if method == "theory" and family == "sibling").
+##' @param kappa: vector of correlation between G and Gsib for each SNP. Default is 0 (only used if method = "theory" and family == "sibling").
+##'
+##' @return a data frame of snp id, calibrated beta and its standard error
+calibration_summary_stats <- function(df, rho = NA, family = "trio", method =  "empirical", r = NA,  
+                                      pheno_cor = NA, family_size = 2, kappa = .5){
+  
+  n = df$N_int
+  N = df$N_ext
+  
+  if (is.na(rho)){
+    noise_cor = noise_correlation(df)
+  } else{
+    noise_cor = rho
+  }
+  
+  # Compute value and variance of alpha_int - alpha_ext
+  alpha_diff = df$alpha_ext - df$alpha_int
+  alpha_diff_var = df$alpha_int_var + df$alpha_ext_var - 2 * noise_cor * sqrt(df$alpha_int_var * df$alpha_ext_var )
+  
+  # Standardize estimates
+  beta_int_std = df$beta_int / sqrt(df$beta_int_var)
+  alpha_diff_std = alpha_diff / sqrt(alpha_diff_var)
+  
+  if(family == "trio"){
+    rho = noise_cor * sqrt(n/N)
+    
+    if(method == "theory"){{
+      if (is.na(r)){
+        beta_alpha_cor = -(1 - rho) / sqrt( 2 * (1 + n/N - 2 * rho)  )
+      } else{
+        beta_alpha_cor = -( (1 - rho) * sqrt(1 - r) )/ sqrt( 2 * (1 + n/N - 2 * rho)  )
+      }
+    }} else {
+      beta_alpha_cor = mean(beta_int_std* alpha_diff_std)
+    }
+  } else{
+    
+    if(method == "theory"){
+      x = 1 + (family_size - 1) * pheno_cor * kappa
+      beta_alpha_cor = -(N / sqrt( N * ( (N * x + n))) ) * (1 - pheno_cor) * sqrt( (family_size - 1) * (1 - kappa) / family_size) / sqrt(1 - pheno_cor/(family_size - 1))
+    } else{
+      beta_alpha_cor = mean(beta_int_std* alpha_diff_std)
+    }
+  }
+  beta_alpha_cov = beta_alpha_cor * sqrt(df$beta_int_var) * sqrt(alpha_diff_var)
+  
+  lambda = - beta_alpha_cov  / alpha_diff_var
+  beta_cal = df$beta_int + lambda * alpha_diff
+  
+  beta_cal_var = df$beta_int_var + lambda^2 * alpha_diff_var + 2 * lambda * beta_alpha_cov
+  return(data.frame(SNP = df$SNP, beta_cal = beta_cal, beta_cal_var = beta_cal_var, lambda = lambda))
+  
+}
